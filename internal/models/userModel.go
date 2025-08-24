b92f9afe61028fee5e7ef716f29ad78100c4c2fc
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2025-08-24 12:38:16 +0300

    user model

--- internal/models/userModel.go
+++ internal/models/userModel.go
@@ -1,5 +1,23 @@
 package models
 
+// creates a new type called Role whose underlying type is string.
+// Role is not just a plain string anymore - it's a custom type.
+type Role string
+
+const (
+	RoleAdmin  Role = "admin"
+	RoleMember Role = "member"
+)
+
+// RoleAdmin is a constant of type Role with the value "admin".
+// RoleMember is a constant of type Role with the value "member".
+// this gives a stronger type safety. Instead of letting anyone assign just any string to Role, we're forcing it to be either "admin" or "member".
+
 type User struct {
-	
+	BaseModel
+	Firstname string `gorm:"type:varchar(200); not null;" json:"firstname"`
+	Lastname  string `gorm:"type:varchar(200); not null;" json:"lastname"`
+	Email     string `gorm:"type:varchar(100); not null; unique;" json:"email"`
+	Password  string `gorm:"type:varchar(200); not null;" json:"-"`
+	Role      Role   `gorm:"type:enum('admin', 'member'); default:'member'" json:"role"`
 }
